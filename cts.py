c4956be9ff77fcc13e7035d3b2424d2bfa78af2b
Adam-Grzenda <65490875+Adam-Grzenda@users.noreply.github.com>
2022-03-28 23:29:19 +0200

    refactor

--- cts.py
+++ cts.py
@@ -5,7 +5,7 @@ from PIL import Image
 import utilities
 from tomograph import Tomograph, make_result_image, make_sinogram
 
-ALLOWED_IMAGE_FORMATS = {"jpeg", "png", "jpg", "dcm"},
+ALLOWED_IMAGE_FORMATS = {"jpeg", "png", "jpg", "dcm"}
 DICOM_FORMAT = "dcm"
 
 
@@ -17,60 +17,80 @@ class Sliders:
         self.boundary = st.sidebar.slider('Boundary angle [degrees]', 0, 360, 360)
 
 
-def handle_dicom_file(st, file):
+class InputFields:
+    def __init__(self, container):
+        self.patients_id = container.text_input("Patient's ID:", )
+        self.patients_name = container.text_input("Patient's name and surname:")
+        self.examination_date = container.date_input("Examination date:")
+
+
+def handle_dicom_file(file):
     dicom = utilities.extract_dicom_data(file)
     st.session_state.image = dicom.image
+
+
+def handle_upload():
+    file = st.session_state.uploaded_file
+    file_extension = st.session_state.uploaded_file.name.split(".")[1]
+    if file_extension in ALLOWED_IMAGE_FORMATS:
+        st.session_state['image'] = Image.open(st.session_state.uploaded_file)
+    elif file_extension == DICOM_FORMAT:
+        handle_dicom_file(file)
+    else:
+        print("Unexpected file format", file_extension)
+
+
+def run_and_display(sinogram_container, tomograph_container):
+    image_array, st.session_state['radius'] = utilities.read_image(st.session_state.image)
+
+    tomograph = Tomograph(sliders.detectors_number, sliders.detection_angle, sliders.step, st.session_state.radius)
+    sinogram = make_sinogram(image_array, tomograph, sliders.boundary)
+
+    sinogram_image = Image.fromarray(np.array(sinogram))
+    sinogram_image = sinogram_image.convert('RGB')
+
+    sinogram_container.image(sinogram_image, width=350)
+
+    tomograph_image = make_result_image(sinogram, tomograph, st.session_state.radius, sliders.boundary)
+    tomograph_image = Image.fromarray(tomograph_image)
+    tomograph_image = tomograph_image.convert('RGB')
+
+    tomograph_container.image(tomograph_image, width=350, clamp=True)
+
+
+def checkFileUploaded():
+    if st.session_state.uploaded_file is not None:
+        handle_upload()
+
+
+def init_session_state(previewContainer):
+    if 'image' not in st.session_state:
+        st.session_state.image = None
+    if 'radius' not in st.session_state:
+        st.session_state.radius = None
+
+    if st.session_state.image is not None:
+        previewContainer.text("Imported image")
+        previewContainer.image(st.session_state.image)
 
 
 if __name__ == '__main__':
     st.title('Computed Tomography Simulator')
     st.sidebar.header("Control panel")
 
-    if 'image' not in st.session_state:
-        st.session_state['image'] = None
-    if 'radius' not in st.session_state:
-        st.session_state['radius'] = None
+    st.session_state.uploaded_file = st.sidebar.file_uploader("Upload your file")
+    checkFileUploaded()
 
-    st.session_state['uploaded_file'] = st.sidebar.file_uploader("Upload your file")
-    if st.session_state.uploaded_file is not None:
-        file = st.session_state.uploaded_file
-        file_extension = st.session_state.uploaded_file.name.split(".")[1]
+    mainContainer = st.container()
+    inputColumn, previewColumn = mainContainer.columns(2)
 
-        if file_extension in ALLOWED_IMAGE_FORMATS:
-            st.session_state['image'] = Image.open(st.session_state.uploaded_file)
-        elif file_extension == DICOM_FORMAT:
-            handle_dicom_file(st, file)
-        else:
-            print("Unexpected file format {}", file_extension)
-
-    container1 = st.container()
-    col1, col2 = container1.columns(2)
+    init_session_state(previewColumn)
 
     sliders = Sliders()
+    inputs = InputFields(inputColumn)
 
-
-    patients_id = col1.text_input("Patient's ID:", )
-    patients_name = col1.text_input("Patient's name and surname:")
-    examination_date = col1.date_input("Examination date:")
-
-    if st.session_state.image is not None:
-        col2.text("Imported image")
-        col2.image(st.session_state.image)
-
-    container2 = st.container()
-    col21, col22 = container2.columns(2)
+    resultContainer = st.container()
+    sinogram, tomograph = resultContainer.columns(2)
 
     if st.sidebar.button("Run") and st.session_state.image is not None:
-        image_array, st.session_state['radius'] = utilities.read_image(st.session_state.image)
-
-        tomograph = Tomograph(sliders.detectors_number, sliders.detection_angle, sliders.step, st.session_state.radius)
-        sinogram = make_sinogram(image_array, tomograph, sliders.boundary)
-
-        sinogram_to_display = Image.fromarray(np.array(sinogram))
-        sinogram_to_display = sinogram_to_display.convert('RGB')
-        col21.image(sinogram_to_display, width=350)
-
-        result_image = make_result_image(sinogram, tomograph, st.session_state.radius, boundary)
-        result_image = Image.fromarray(result_image)
-        result_image = result_image.convert('RGB')
-        col22.image(result_image, width=350, clamp=True)
+        run_and_display(sinogram_container=sinogram, tomograph_container=tomograph)
